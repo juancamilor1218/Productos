fad0d9d97416459e756a67f13ecdc0e784916ecc
Esteban_Dario_Jojoa_Rosero <estebandariojojoa9@gmail.com>
2018-05-24 13:13:13 -0500

    esteban commit

--- app/src/main/java/com/example/acer/edittext/Products/Interactor/MainActivityInteractor.java
+++
@@ -1,9 +0,0 @@
-package com.example.acer.edittext.Products.Interactor;
-
-/**
- * Created by Acer on 15/05/2018.
- */
-
-public interface MainActivityInteractor {
-    void sumar(String n1,String n2);
-}
